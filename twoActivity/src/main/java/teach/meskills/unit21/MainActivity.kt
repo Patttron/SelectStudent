ec9575e6497c009165a3063fdcf15c9e5742d647
Patttron <Realisttt19@gmail.com>
2022-01-31 15:57:03 +0300

    Fixed bugs

--- twoActivity/src/main/java/teach/meskills/unit21/MainActivity.kt
+++ twoActivity/src/main/java/teach/meskills/unit21/MainActivity.kt
@@ -13,17 +13,21 @@ import kotlin.random.Random
 class MainActivity : AppCompatActivity() {
     private lateinit var chosen: TextView
 
-    private val getContent = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
-        activityResult ->
-        val resultCode = activityResult.resultCode
-        val data = activityResult.data
-        if (resultCode == RESULT_OK) {
-            val students = data?.getStringArrayListExtra(EXTRA)
-            findViewById<Button>(R.id.roll).setOnClickListener {
-                chosen.text = students?.get(Random.nextInt(students.size))
+    private val getContent =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { activityResult ->
+            val resultCode = activityResult.resultCode
+            val data = activityResult.data
+            if (resultCode == RESULT_OK) {
+                val students = data?.getStringArrayListExtra(EXTRA)
+                findViewById<Button>(R.id.roll).setOnClickListener {
+                    if (students!!.isNotEmpty()) {
+                        chosen.text = students?.get(Random.nextInt(students.size))
+                    } else {
+                        chosen.text = "Choose some student"
+                    }
+                }
             }
         }
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,6 +35,11 @@ class MainActivity : AppCompatActivity() {
         chosen = findViewById(R.id.chosen)
         findViewById<Button>(R.id.choose).setOnClickListener {
             getContent.launch(Intent(this, Checkboxes::class.java))
+        }
+        findViewById<Button>(R.id.roll).setOnClickListener {
+            if (chosen.text.isEmpty()) {
+                chosen.text = "Choose some student"
+            }
         }
     }
 }
